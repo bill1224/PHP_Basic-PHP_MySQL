f1679005f912dfa078aa74a4ff6e02833f3431c9
shtmd6171 <samantha.n.morton@gmail.com>
2020-07-27 02:15:08 +0900

    create 부분이 조건에 의해 표시되도록 수정, 업데이트 페이지 이동시 ID값 수반

--- index_.php
+++ index_.php
@@ -38,6 +38,16 @@ function print_description() {
      <ol>
        <?php print_list();?>
      </ol>
+     <a href="create.php">create</a>
+     <!-- html문은 php문 밖으로 제외시키고 } 부분을
+         html문으로 묶어 if문에 포함되게 했다.
+        메인페이지에는 업데이트가 뜨지 않도록 만든다. -->
+     <?php if (isset($_GET['id'])) { ?>
+      <!-- <\?php echo 의 형태로 적어야 하는 경우에는 (주석처리 때문에 이스케이프 넣음)
+           아래와 같은 <\?=의 형식으로 적어도 된다.
+           업데이트 시에는 업데이트 페이지로 이동했을 때, 해당 ID값을 가져가게 한다. -->
+      <a href="update.php?id=<?= $_GET['id'];?>">update</a>
+     <?php } ?>
      <h2>
        <?php print_title(); ?>
      </h2>
