f9b8f3ead0e8eabc0e1590cae8758f095da69dd4
shtmd6171 <samantha.n.morton@gmail.com>
2020-07-26 23:32:01 +0900

    PHP 문법 추가 ~ 5번

---
+++ PHP_Syntax_Basic/PHP_Syntax.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    여기서 적어낸 부분들은 배우는 도중 몰랐던 문법을 작성하는 구간으로<br>
+    이미 알고 있는 것 외의 문법만을 표시합니다<br>
+
+    <?php
+    $b = "<br>"."<br>";
+
+    // 1 ) URL param을 통해 값을 표현하는 방법
+    //http://127.0.0.1/PHP/PHP_Syntax_Basic/PHP_Syntax.php?name=PHP
+    echo $_GET['name'].$b;
+
+    $string = "Today is gonna be the day
+    That they're gonna throw it back to you
+    By now you should've somehow
+
+
+    Realized what you gotta do
+    I don't believe that anybody
+    Feels the way I do, about you now";
+    // 2 ) nl2br()을 이용하면 <pre>처럼 string 개행 상태 그대로 출력한다.
+    echo nl2br($string).$b;
+
+    // 3 ) var_dump()를 이용하면 해당 값과 타입 기타 정보를 표시해준다.
+    echo var_dump("hello world").$b;
+
+    // 4 ) scandir(파일경로)를 통해 파일 목록을 read 한다.
+    // node.js 의 readdir(파일경로, function(err,filelist){})
+    /* scandir을 통해 배열이 생성되고 0,1 index는 현재, 상위 디렉터리 내용을 포함한다.
+    { [0]=> string(1) "." [1]=> string(2) ".." [2]=> string(3) "CSS"
+      [3]=> string(4) "HTML"  [4]=> string(10) "JavaScript" }*/
+    $list = scandir('../data');
+    echo var_dump($list).$b;
+
+    // 5 ) file_get_contents(파일경로)를 통해 파일 내용을 read 한다.
+    // node.js 의 fileread(파일경로, 'utf-8', function(err,data){})
+     echo file_get_contents("../data/".$_GET['name']).$b;
+
+
+
+     ?>
+  </body>
+</html>
