fae637ad0f8b2611a1838e2af31c81e4cc59097b
shtmd6171 <samantha.n.morton@gmail.com>
2020-07-30 03:23:06 +0900

    MYSQLI를 이용한 PHP연동, INSERT

--- PHP_MySQL/MySQL_API_Syntax/insert.php
+++ PHP_MySQL/MySQL_API_Syntax/insert.php
@@ -3,6 +3,8 @@
 // 이 중에서 mysqli  방식을 이용해서 구성한다.
 // mySQL 연동 방식을 함수(procedural)을 사용한다. 이 외에 객체방식이 존재한다.
 
+// 이 Syntax 파일에서는 MYSQL과의 연동 / Query문을 통한 DB의 INSERT 내용을 설명하고 있다.
+
 // mysqli_connect('DB주소', 'USERNAME', "PW", "DatabeseNAME");
 // DB주소 : php와 DB가 같은 주소에 있으므로 localhost로 대체한다.
 // USERNAME : Mysql에서 사용하는 사용자 이름
