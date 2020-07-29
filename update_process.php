e32f552e71de757b8e294a5994959e2d8a0e28d1
shtmd6171 <samantha.n.morton@gmail.com>
2020-07-30 01:05:49 +0900

    파일 위치 변경으로 인한 업데이트 파일 삭제

--- update_process.php
+++
@@ -1,5 +0,0 @@
-<?php
- rename('data/'.$_POST['oldtitle'],'data/'.$_POST['title']);
- file_put_contents('data/'.$_POST['title'], $_POST['desc']);
- header("Location: index_.php?id=".$_POST['title']);
- ?>
