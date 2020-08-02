d0922d884acb476accffd7d667adc9efc5cbbb13
shtmd6171 <samantha.n.morton@gmail.com>
2020-08-02 17:49:34 +0900

    created table readme

--- PHP_MySQL/PHP_MYSQL_WEB/LECTURE_WEB_SERVER/db.php
+++ PHP_MySQL/PHP_MYSQL_WEB/LECTURE_WEB_SERVER/db.php
@@ -1,6 +1,16 @@
+<!--
+in test (database)
+
+CREATE TABLE `member`
+( `u_count` int(15) NOT NULL, `userid` varchar(18) NOT NULL, `userpw` varchar(60) NOT NULL, `name` varchar(18) NOT NULL,
+`age` int(5) NOT NULL, `sex` varchar(10) NOT NULL, `code` varchar(2) NOT NULL DEFAULT 'U' )
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-->
+
  <?php
   session_start();
-  
+
   $db = new mysqli("localhost","root","","test");
   $db->set_charset("utf8");
 
