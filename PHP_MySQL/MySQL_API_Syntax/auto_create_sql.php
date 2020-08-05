edc385719ee26aa88465f470959d1b6484ecbbd4
shtmd6171 <samantha.n.morton@gmail.com>
2020-08-05 09:36:52 +0900

    SQL 자동생성

---
+++ PHP_MySQL/MySQL_API_Syntax/auto_create_sql.php
@@ -0,0 +1,36 @@
+<?php
+include "../PHP_MYSQL_WEB/LECTURE_WEB_SERVER/lib/db.php";
+?>
+
+<form  method="post">
+  <input type="submit" name="test" id="id_value" value="RUN"><br>
+</form>
+
+
+<?php
+  function testfun() {
+    $mk_table_sql = fopen("./test.sql","r");
+
+    $sql_line= '';
+    while (!feof($mk_table_sql)) {
+      $line_of_text = fgets($mk_table_sql);
+      $sql_line = "$sql_line"."$line_of_text";
+      echo "$line_of_text"."<br>";
+    }
+
+    fclose($mk_table_sql);
+
+    echo "<br>";
+    echo $sql_line;
+    $sql = mq("$sql_line");
+    echo "<br>";
+    echo "<pre>";
+    var_dump($sql);
+
+  }
+
+  if(array_key_exists('test',$_POST)) {
+     testfun();
+  }
+
+ ?>
---
+++ PHP_MySQL/MySQL_API_Syntax/test.sql
@@ -0,0 +1,8 @@
+create table testing (
+ sno int(11) NOT NULL,
+ name char(10) DEFAULT NULL,
+ det char(10) DEFAULT NULL,
+ addr char(10) DEFAULT NULL,
+ tel char(10) DEFAULT NULL,
+ PRIMARY KEY (sno)
+);
